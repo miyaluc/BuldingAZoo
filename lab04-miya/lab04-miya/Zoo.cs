699e03528cbb45652d7471c5a624cddb459fb256
miyaluc <srlucas3@gmail.com>
2017-09-15 12:05:20 -0700

    added classes to my Zoo program added a few methods as well still need to work on abstraction

---
+++ lab04-miya/lab04-miya/Zoo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab04-miya
+{
+    class Zoo
+    {
+    public string Africa
+    {
+        get; set;
+    }
+    public string NorthAmerica
+    {
+        get; set;
+    }
+    public string Asia
+    {
+        get; set;
+    }
+    public string Antarctica
+    {
+        get; set;
+    }
+
+    public Zoo(string continent)
+    {
+        Africa = continent;
+    }
+    public void Eat()
+    {
+
+    }
+    public void Sleep()
+    {
+
+    }
+    public bool IsPrey()
+    {
+        bool prey = true;
+        return prey;
+    }
+
+}
